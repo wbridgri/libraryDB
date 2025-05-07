015b52eb424dc5a7375c29abd571bd195398b6a9
Will Bridges <106503676+wbridgri@users.noreply.github.com>
2025-05-06 23:45:28 -0500

    titulos

---
+++ schema.sql
@@ -0,0 +1,27 @@
+drop database if exists libraryDB;
+create database libraryDB;
+use libraryDB;
+
+create table(
+1	Blackouts
+2	The Floating World
+3	Little Thieves
+4	Pride or Die
+5	Dead Wednesday
+6	Dead Things are Closer Than They Aooear
+7	The Co-op
+8	Starter Villain
+9	Harriet Tubman: Live in Concert
+10	The Party
+11	Mr. Mercedes
+12	Blood of Elves
+13	The Art of Fear
+14	Behind Her Eyes
+15	The Cirruotion of Hollis Brown
+16	Desert Echoes
+17	The Prison Healer
+18	The Witch's Heart
+19	The Tale of Genji
+20	The Last Wish
+	
+)
