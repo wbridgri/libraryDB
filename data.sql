0837cd407f7da676c72812034a338b36f7bd25dc
Will Bridges <106503676+wbridgri@users.noreply.github.com>
2025-05-07 18:13:18 -0500

    publisher done

---
+++ data.sql
@@ -0,0 +1,45 @@
+insert into author(author)
+     ('Justin Torres');
+
+insert into author(author)
+    ('Axie Oh'),
+('Margret Owen'),
+('CL Montblanc'),
+('Jerry Spinelli'),
+('Robin Wasley'),
+('Tarah DeWitt'),
+('John Scalzi'),
+('Bob the Drag Queen'),
+('Natasha Preston'),
+('Stephen King'),
+('Andrzej Sapokowski'),
+('Pamela Crane'),
+('Sarah Pinborough'),
+('K. Ancrum'),
+('Abdi Nazemian'),
+('Lynette Noni'),
+('Genevieve Gornichec'),
+('Murasaki Shikibu');
+
+insert into publisher(publisher)
+     ('Picador');
+
+insert into publisher(publisher)
+    values('Feiwel & Friends'), 
+    ('St. Martin''s'), 
+    ('Yearling'), 
+    ('Simon & Schuster'), 
+    ('Self Published'), 
+    ('Tor Books'), 
+    ('Knopf'), 
+    ('Gallery Books'),
+    ('Delacorte'),
+    ('Scribner'),
+    ('Hachette Book Group'),
+    ('Rockin'' C Reads'),
+    ('Flatiron Books'),
+    ('HarperCollins'),
+    ('Clarion Books'),
+    ('Ace'),
+    ('Penguin'),
+    ('Orbit');
