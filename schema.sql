d20032049fc47673fed227c9fc5afe5457ae9358
Will Bridges <106503676+wbridgri@users.noreply.github.com>
2025-05-07 14:17:15 -0500

    trying to complete tables

--- schema.sql
+++ schema.sql
@@ -5,6 +5,8 @@ use libraryDB;
 create table author(
     author_id TINYINT unsigned AUTO_INCREMENT NOT NULL,
     author varchar(40),
+    first_name varchar(20),
+    last_name varchar(20)
     constraint pk_author primary key (author_id)
 );
 
@@ -18,10 +20,23 @@ create table genre(
     genre_id tinyint unsigned AUTO_INCREMENT NOT NULL,
     genre varchar(40),
     constraint pk_genre primary key (genre_id)
-)
+);
 
 create table format(
-    format_id unsigned AUTO_INCREMENT NOT NULL,
-    format varchar(15),
-    contraint pk_format primary key (format_id)
+    format_id tinyint unsigned AUTO_INCREMENT NOT NULL,
+    format ENUM ('PHYSICAL', 'AUDIOBOOK', 'E-BOOK'),
+    constraint pk_format primary key (format_id)
 );
+
+create table book(
+    book_id tinyint unsigned AUTO_INCREMENT not null,
+    title varchar(60),
+    author_id tinyint unsigned AUTO_INCREMENT not null,
+    pub_id tinyint unsigned AUTO_INCREMENT not null,
+    copyright_year YEAR,
+    edition tinyint unsigned NOT NULL,
+    edition_year YEAR
+
+
+)
+
